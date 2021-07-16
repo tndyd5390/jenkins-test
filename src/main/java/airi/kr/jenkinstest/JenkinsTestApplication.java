f915ea23d80d8867994e9b96601d742b6ca033b8
waterdragon <tndyd5390@naver.com>
2021-07-16 16:48:08 +0900

    test

--- src/main/java/airi/kr/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/airi/kr/jenkinstest/JenkinsTestApplication.java
@@ -18,7 +18,7 @@ public class JenkinsTestApplication {
 
   @GetMapping(value="/")
   public String home() {
-    System.out.println("testBranchaaaaaa");
+    System.out.println("testBranchaaaaaaa");
     return "webhook test";
 
   }
